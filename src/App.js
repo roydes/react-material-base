425d645db33e319a059b9bbb65ac74161a39313b
Roides Javier Cruz Lara <roidescruzlara@gmail.com>
2018-09-10 14:09:37 -0500

    Add: Material App Toolbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AppToolBar from "./components/gui-components/navegation-components/toolbar/toolbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppToolBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
